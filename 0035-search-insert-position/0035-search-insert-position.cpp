683e9b94334eb9de5e4fbd52d907911a2088ae1c
Rishabh Nahar <85643869+rishabhnhr18@users.noreply.github.com>
2024-10-10 01:26:55 +0530

    Time: 3 ms (71.1%), Space: 12.3 MB (62.34%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
-    int func(vector<int> no, int low, int high, int target) {
-        if (low > high)
-            return low;
-        int mid = (low + high) / 2;
-        if(target==no[mid]) return mid;
-        else if (target > no[mid]) return func(no, (mid + 1), high, target);
-        else return func(no, low, (mid - 1), target);
-    }
     int searchInsert(vector<int>& nums, int target) {
-        return func(nums, 0, nums.size() - 1, target);
+        return lower_bound(nums.begin(), nums.end(), target)-nums.begin();
     }
 };
