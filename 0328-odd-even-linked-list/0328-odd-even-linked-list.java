06bd3fe70429d2887153e02817607621f37dd6a5
Rishabh Nahar <85643869+rishabhnhr18@users.noreply.github.com>
2024-12-01 13:41:31 +0530

    Time: 0 ms (100%), Space: 45 MB (5.97%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -16,10 +16,10 @@ class Solution {
         ListNode evenHead=head.next; // for adding evenhesd to end of the odd part
         while(even!=null && even.next!=null)
         {
-            odd.next=even.next;
-            odd=odd.next;
-            even.next=odd.next;
-            even=even.next;
+            odd.next=even.next; // connecting the odd indice node with other odd indice after the even node
+            odd=odd.next;   //moving the odd pointer to the newly add odd node 
+            even.next=odd.next; // connecting the even indice node with other even indice node after the odd node
+            even=even.next;  //moving the evenpointer to the newly add even node 
         } 
         odd.next=evenHead;
         return head;
