630ce981f5c0f98ee2e2c5c2d84abe0277dcbf1d
Rishabh Nahar <85643869+rishabhnhr18@users.noreply.github.com>
2024-12-07 14:50:31 +0530

    Time: 13 ms (74.41%), Space: 45.4 MB (74.42%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,19 +1,20 @@
 class Solution {
     public String frequencySort(String s) {
-        if(s==null || s.length()==0) return "";
-        Map<Character, Integer> m=new HashMap<>();
-        StringBuilder str=new StringBuilder();
-        for(char c: s.toCharArray()) m.put(c, m.getOrDefault(c,0)+1);     
-        
-        List<Character> lst=new ArrayList<>(m.keySet());
-        lst.sort((a,b)->m.get(b)-m.get(a));
-        for(char c: lst)
+        if (s == null || s.length() == 0)
+            return "";
+        Map<Character, Integer> m = new HashMap<>();
+        StringBuilder str = new StringBuilder();
+        for (char c : s.toCharArray())
+            m.put(c, m.getOrDefault(c, 0) + 1); // count frequency of elements in strings
+        List<Character> lst = new ArrayList<>(m.keySet()); // created a list containin set of unikeys keys of map
+        lst.sort((a, b) -> m.get(b) - m.get(a)); // sorted by wrinting self written comparator function, sorting
+                                                 // elements in decending order
+        for (char c : lst) // iterating thoughtout the list containing unique elements
         {
-            int count=m.get(c);
-            int i=1;
-            while(i<=count)
-            {
-                str.append(c);
+            int count = m.get(c); // finding count of each unique element of list from map
+            int i = 1;
+            while (i <= count) {
+                str.append(c); // appending the character count times
                 i++;
             }
         }
