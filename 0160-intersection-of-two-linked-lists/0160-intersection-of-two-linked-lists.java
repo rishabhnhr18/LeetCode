9c7019d1754480f9e27701163f90b9438467dd22
Rishabh Nahar <85643869+rishabhnhr18@users.noreply.github.com>
2024-12-13 22:46:40 +0530

    Time: 1 ms (99.91%), Space: 49.1 MB (13.46%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -29,13 +29,54 @@ public class Solution {
          * }
          * return null;
          */
-        // apporach 2
+
+         /*
+        // apporach 2 brute force
         for(ListNode nodeA=headA; nodeA!=null ; nodeA=nodeA.next)
         {
             for(ListNode nodeB=headB; nodeB!=null; nodeB=nodeB.next)
             {
                 if(nodeA==nodeB) return nodeA;
             }
+        }
+        return null;
+        */
+
+    //approach 3
+        ListNode nodeA = headA, nodeB = headB;
+        int lengthA = 1, lengthB = 1;
+        for (; nodeA != null; nodeA = nodeA.next)
+            lengthA++;
+        for (; nodeB != null; nodeB = nodeB.next)
+            lengthB++;
+        int diff =Math.abs(lengthA-lengthB);
+        nodeA = headA;
+        nodeB = headB;
+    if(lengthA>lengthB)
+    {
+        int x=0;
+        while(x<diff)
+        {
+            nodeA=nodeA.next;
+            x++;
+        }
+    }
+    else
+    {
+          int x=0;
+        while(x<diff)
+        {
+            nodeB=nodeB.next;
+            x++;
+        }
+    }
+        if (nodeA == nodeB)
+            return nodeA;
+        while (nodeA != null && nodeB != null) {
+            if (nodeA == nodeB)
+                return nodeA;
+            nodeA = nodeA.next;
+            nodeB = nodeB.next;
         }
         return null;
     }
