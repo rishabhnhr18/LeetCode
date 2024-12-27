875c0c3cdf7c12251eb24ac5211f829f899abf6b
Rishabh Nahar <85643869+rishabhnhr18@users.noreply.github.com>
2024-12-27 20:18:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int maxScoreSightseeingPair(int[] values) {
+        //a[i]+i max
+        //a[j]-j min
+        int maxi=Integer.MIN_VALUE;
+        for(int )
+    }
+}
