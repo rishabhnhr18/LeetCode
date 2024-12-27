04886edc3b71aa9d3d2f6a1433213e2bd828243a
Rishabh Nahar <85643869+rishabhnhr18@users.noreply.github.com>
2024-12-27 20:19:58 +0530

    Time: 4 ms (89.46%), Space: 53.3 MB (68.11%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
@@ -1,8 +1,34 @@
 class Solution {
     public int maxScoreSightseeingPair(int[] values) {
-        //a[i]+i max
-        //a[j]-j min
-        int maxi=Integer.MIN_VALUE;
-        for(int )
+        /*
+         * approach 1 (wrong)
+         * //a[i]+i max
+         * //a[j]-j min
+         * int maxi=Integer.MIN_VALUE;
+         * int mini=Integer.MAX_VALUE;
+         * int ans=maxi;
+         * int x=0;
+         * for(int val: values)
+         * {
+         * maxi=Math.max(maxi, (val+x));
+         * mini=Math.min(mini, (val-x));
+         * ans=Math.max(ans,(maxi+mini));
+         * x++;
+         * }
+         * return ans;
+         */
+
+        // approach 2
+        if (values.length == 0)
+            return 0;
+        int first = values[0]; // initially first is at index 0;
+        int maxi = Integer.MIN_VALUE;
+        for (int j = 1; j < values.length; j++) // started searching from second index
+        {
+            maxi = Math.max(maxi, first + values[j] - j); // finding the maximum score
+            first = Math.max(first, values[j] + j); // if the second index is larger than first index, so making it as
+                                                    // first index to maximize the score
+        }
+        return maxi;
     }
 }
