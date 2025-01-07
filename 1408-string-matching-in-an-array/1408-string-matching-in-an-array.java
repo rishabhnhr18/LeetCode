6c37403d04c8bec9b57bd249812454a57709c5d5
Rishabh Nahar <85643869+rishabhnhr18@users.noreply.github.com>
2025-01-07 22:48:42 +0530

    Time: 5 ms (41.61%), Space: 42.3 MB (37.89%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
@@ -1,21 +1,39 @@
 class Solution {
     public List<String> stringMatching(String[] words) {
-        if(words==null || words.length==0) return new ArrayList<String>();
-        Set<String> res=new HashSet<>();
-        for(int i=0; i<words.length; i++)
-        {   for(int j=i+1; j<=words.length-1; j++)
-            {
-                if(words[i].contains(words[j])) res.add(words[j]);
-            }
-        }
-        for(int i=words.length-1; i>=0; i--)
-        {
-               for(int j=i-1; j>=0; j--)
-            {
-                if(words[i].contains(words[j])) res.add(words[j]);
-            }
-        }
-        return new ArrayList<String>(res);
+        if (words == null || words.length == 0)
+            return new ArrayList<String>();
+        /*
+         * Approach 1 correct
+         * Set<String> res=new HashSet<>();
+         * for(int i=0; i<words.length; i++)
+         * { for(int j=i+1; j<=words.length-1; j++)
+         * {
+         * if(words[i].contains(words[j])) res.add(words[j]);
+         * }
+         * }
+         * for(int i=words.length-1; i>=0; i--)
+         * {
+         * for(int j=i-1; j>=0; j--)
+         * {
+         * if(words[i].contains(words[j])) res.add(words[j]);
+         * }
+         * }
+         * return new ArrayList<String>(res);
+         */
 
+        // approach 2
+
+        Set<String> res = new HashSet<>();
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i; j < words.length; j++) {
+                if (i != j) {
+                    if (words[i].contains(words[j]))
+                        res.add(words[j]);
+                   else if(words[j].contains(words[i]))
+                        res.add(words[i]);
+                }
+            }
+        }
+        return new ArrayList<>(res);
     }
 }
