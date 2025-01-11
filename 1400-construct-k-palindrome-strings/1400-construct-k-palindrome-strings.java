df8671a1b9f00fdab0516fc0d7934761eb942eb0
Rishabh Nahar <85643869+rishabhnhr18@users.noreply.github.com>
2025-01-12 03:31:17 +0530

    Time: 28 ms (22.4%), Space: 45.7 MB (19.16%) - LeetHub

--- 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.java
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean canConstruct(String s, int k) {
-        if(s==null || k==0 || s.length()==0) return false;
+        if(s==null || k==0 || s.length()==0 || s.length()<k) return false;
         if(s.length()==k) return true;
-        if(s.length()<k) return false;
         Map<Character, Integer> map=new HashMap<>();
         for(char c: s.toCharArray()) map.put(c, map.getOrDefault(c, 0) + 1);
         int count=0;
@@ -10,7 +9,6 @@ class Solution {
         {
          if(entry.getValue()%2!=0) count++;   
         }
-        if(count<=k) return true;
-        return false;
+        return count<=k;
     }
 }
