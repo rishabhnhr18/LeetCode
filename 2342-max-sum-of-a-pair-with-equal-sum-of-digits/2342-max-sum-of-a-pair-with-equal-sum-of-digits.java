ab8d93ab017ae2d3016ec8082268c45c9ad11c8b
Rishabh Nahar <85643869+rishabhnhr18@users.noreply.github.com>
2025-02-13 00:14:05 +0530

    Time: 50 ms (53.95%), Space: 63.2 MB (22.32%) - LeetHub

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -6,53 +6,48 @@
 class Solution {
     public int maximumSum(int[] nums) {
         int max = -1;
-/*
-        Approach 1 tle error
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                int num1 = nums[i];
-                int num2 = nums[j];
-                int res1 = 0, res2 = 0;
-                for (; num1 > 0; num1 = num1 / 10)
-                    res1 += num1 % 10;
-                for (; num2 > 0; num2 = num2 / 10)
-                    res2 += num2 % 10;
-                if (res1 == res2)
-                    max = Math.max(max, nums[i] + nums[j]);
-            }
-        }
-        return max;
-        */
-        
-        
         /*
-            Approach 2  Using HashMap -> tore the sum of digits of each number if got same calcuate furthur, else add to map :)
-
-        */
-
-        Map<Integer, Integer> map= new HashMap<>();
-        for(int num: nums)
-        {
-        int n = findDigitSum(num);
-        if(map.containsKey(n))
-        {
-            max= Math.max(max, map.get(n)+num);
-            int x= Math.max(num , map.get(n));
-            map.remove(n);
-            map.put(n, x);
-        }
-        else map.put(n, num);
+         * Approach 1 tle error
+         * for (int i = 0; i < nums.length - 1; i++) {
+         * for (int j = i + 1; j < nums.length; j++) {
+         * int num1 = nums[i];
+         * int num2 = nums[j];
+         * int res1 = 0, res2 = 0;
+         * for (; num1 > 0; num1 = num1 / 10)
+         * res1 += num1 % 10;
+         * for (; num2 > 0; num2 = num2 / 10)
+         * res2 += num2 % 10;
+         * if (res1 == res2)
+         * max = Math.max(max, nums[i] + nums[j]);
+         * }
+         * }
+         * return max;
+         */
+//-------------------------------------------------------------------------------
+        /*
+         * Approach 2 Using HashMap -> tore the sum of digits of each number if got same
+         * calcuate furthur, else add to map :)
+         */
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            int n = findDigitSum(num);
+            if (map.containsKey(n)) {
+                max = Math.max(max, map.get(n) + num);
+                int x = Math.max(num, map.get(n));
+                map.put(n, x);
+            } else
+                map.put(n, num);
         }
         return max;
     }
-    private int findDigitSum(int num)
-    {
-        if(num<=0) return 0;
-        int res=0;
-        while (num> 0)
-        {
-            res+=num%10;
-            num/=10;
+
+    private int findDigitSum(int num) {
+        if (num <= 0)
+            return 0;
+        int res = 0;
+        while (num > 0) {
+            res += num % 10;
+            num /= 10;
         }
         return res;
     }
